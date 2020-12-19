5588daac01b0273e5eab95a24a6b6081a2ff4fae
Sergey Tarsenko <67330524+TatisWeb@users.noreply.github.com>
2020-12-19 14:26:09 +0400

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -24,3 +24,24 @@ $(document).ready(function() {
  });
 
 });
+
+// button menu burger open
+
+$('.menu-btn').on('click', function(e) {
+  e.preventDefault();
+  $('.menu').toggleClass('menu_active');
+  $('.content').toggleClass('content_active');
+});
+
+$('.menu-btn').on('click', function(m) {
+ m.preventDefault();
+ $(this).toggleClass('menu-btn_active');
+});
+
+//button menu burger close
+
+$('.menu-close-btn').on('click', function(u) {
+ u.preventDefault();
+ $('.menu').removeClass('menu_active');
+ $('.content').removeClass('content_active');
+ $('.menu-btn').removeClass('menu-btn_active'); });
